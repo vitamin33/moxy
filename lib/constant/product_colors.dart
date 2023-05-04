7abf29d15bd41715ff326a7882419993ec949f34
Stas Kostra <staskostra14@gmail.com>
2023-05-04 13:07:30 +0300

    add-dropdownbutton

---
+++ lib/constant/product_colors.dart
@@ -0,0 +1,15 @@
+List<String> productColors = const [
+  'Black',
+  'White',
+  'Grey',
+  'Pink',
+  'PinkLeo',
+  'Leo',
+  'Brown',
+  'Beige',
+  'Purple',
+  'Zebra',
+  'Jeans',
+  'Green',
+  'Bars',
+];
