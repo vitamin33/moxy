0acbc67c28eba6882d0b1679bd68c12f1f317561
Stas Kostra <staskostra14@gmail.com>
2023-05-31 20:43:41 +0300

    fixed all products ui

--- lib/screens/dashboard/dashboard_view_mobile.dart
+++ lib/screens/dashboard/dashboard_view_mobile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moxy/constant/icon_path.dart';
-import 'package:moxy/domain/dashboard/dashboard_cubit.dart';
 import 'package:moxy/navigation/home_router_delegate.dart';
 import 'package:moxy/theme/app_theme.dart';
 import '../../components/app_scaffold.dart';
@@ -27,8 +26,32 @@ class DashboardViewMobile extends StatelessWidget {
   }
 
   Widget mobileWidget(HomeRouterState state) {
+    final arrIcon = _mapStateToActionIcon(state);
     return AppScaffold(
         appbar: AppBar(
+          actions: [
+            arrIcon.isNotEmpty
+                ? SizedBox(
+                    width: 70,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: arrIcon.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: SvgPicture.asset(arrIcon[index]),
+                                );
+                              }),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container()
+          ],
           backgroundColor: AppTheme.pink,
           title: Text(_mapStateToTitleText(state)),
           leading: Builder(
@@ -79,5 +102,27 @@ class DashboardViewMobile extends StatelessWidget {
         return 'Feedbacks';
     }
     return 'Dashboard';
+  }
+
+  List _mapStateToActionIcon(HomeRouterState state) {
+    switch (state.runtimeType) {
+      case OverviewPageState:
+        return [];
+      case ProductsPageState:
+        return [IconPath.plus, IconPath.filter];
+      case CustomersPageState:
+        return [];
+      case OrdersPageState:
+        return [];
+      case CreateProductPageState:
+        return [];
+      case CreateOrderPageState:
+        return [];
+      case TransactionsPageState:
+        return [];
+      case FeedbacksPageState:
+        return [];
+    }
+    return [];
   }
 }
