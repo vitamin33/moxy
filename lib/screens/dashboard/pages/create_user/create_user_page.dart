6ab554a879e86ca867c9d3cf4ec0ea5a750834c6
Vitalii Serbyn <vivitseser@gmail.com>
2023-06-04 12:21:41 +0300

    Temp

--- lib/screens/dashboard/pages/create_user/create_user_page.dart
+++ lib/screens/dashboard/pages/create_user/create_user_page.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
-class CreateUserPage extends StatelessWidget {
-  const CreateUserPage({Key? key}) : super(key: key);
+class About extends StatelessWidget {
+  const About({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('Create user page'),
-    );
+    return BlocBuilder<CreateOrderCubit, CreateOrderState>(
+        builder: (context, state) {
+      final cubit = context.read<CreateOrderCubit>();
+      return Padding(
+        padding: const EdgeInsets.all(AppTheme.cardPadding),
+        child: Column(
+          children: [
+            CustomTextField(
+              title: ' First Name',
+              maxLines: 1,
+              controller: cubit.firstNameController,
+              onChanged: cubit.firstNameChanged,
+              validation: true,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomTextField(
+              title: 'Second Name',
+              maxLines: 1,
+              controller: cubit.secondNameController,
+              onChanged: cubit.secondNameChanged,
+              validation: true,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomTextField(
+              title: 'Phone Number',
+              controller: cubit.phoneNumberController,
+              onChanged: cubit.phoneNumberChanged,
+              state: state.errors.costPrice,
+              maxLines: 1,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(16),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
