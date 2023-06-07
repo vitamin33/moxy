dc524ed1195bef4089830283bdf5622933767844
Vitalii Serbyn <vivitseser@gmail.com>
2023-06-07 23:07:49 +0300

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:json_theme/json_theme.dart';
-import 'package:moxy/firebase_options.dart';
 import 'package:moxy/moxy_app.dart';
 import 'package:moxy/services/get_it.dart';
 import 'package:moxy/utils/common.dart';
@@ -18,9 +16,7 @@ void main() {
   runZonedGuarded<Future<void>>(() async {
     await dotenv.load(fileName: Environment.fileName);
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+
     final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
     final themeDarkStr =
         await rootBundle.loadString('assets/appainter_theme.json');
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
 import flutter_secure_storage_macos
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
